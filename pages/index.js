5087cefb4db4b7336cb51cfb93b78369db86fc72
aliff <alifzulkifeli@gmail.com>
2021-02-24 19:56:19 +0900

    pub

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Head from 'next/head'
-import LandingPage from './LandingPage';
+import LandingPage from '../components/LandingPage';
+import LamanUtama from '../components/LamanUtama';
 
 export default function Home() {
   return (
-  <>
-    <LandingPage/>
-    <section className="h-screen  bg-gray-300 " >
+    <>
+      <LandingPage />
+      <LamanUtama />
+    </>
 
-    </section></>
- 
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,21 @@
 module.exports = {
-  purge:[],// ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  purge: [],// ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: { 
+    extend: {
       colors: {
-      likepink:'#fdcbcb',
-      likeblue:'#7a80be'
-    }
-  }, 
-   
+        likepink: '#fdcbcb',
+        likeblue: '#7a80be',
+        likepurple: '#a3a8dc',
+        likestrongblue: "#1862a8"
+      }
+    },
+
+  },
+  fontFamily: {
+    "poppins": ['Poppins', "sans-serif"],
+    "purple": ['Purple Purse', "cursive"],
+    "quandro": ['Quando', "serif"]
   },
   variants: {
     extend: {},
